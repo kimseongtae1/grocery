a7a34612f955ca6d5b6a087e291686b2c0b687bf
kimseongtae1 <tjdxo7745@gmail.com>
2023-11-24 10:12:59 +0900

    ff

---
+++ app/api/route.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: process.env.S_HOST,
+	user: process.env.S_USER,
+	password: process.env.S_PASSWORD,
+	database: process.env.S_DATABASE,
+	port: process.env.S_PORT
+});
+
+
+
+connection.connect();
+
+
+export async function queryExecute(str, value) {
+	let data = await new Promise((resolve, reject) => {
+		connection.query(str, value, function (error, results) {
+			resolve(results);
+		});
+	});
+	return data;
+}
